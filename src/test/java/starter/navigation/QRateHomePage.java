c84a6d379b913caa48e2c748746018fe9d0c7c0f
Sreekanth <msaikanth99@gmail.com>
2024-07-27 16:00:22 +0530

    first commit

--- src/test/java/starter/navigation/QRateHomePage.java
+++
@@ -1,8 +0,0 @@
-package starter.navigation;
-
-import net.serenitybdd.annotations.DefaultUrl;
-import net.thucydides.core.pages.PageObject;
-
-@DefaultUrl("https://qrateonline-uat.qqpay.my/login")
-public class QRateHomePage extends PageObject {
-}
