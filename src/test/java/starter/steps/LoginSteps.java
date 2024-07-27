03c7031266d40c798dc93798d735807cf6dc74c8
Sreekanth <msaikanth99@gmail.com>
2024-07-27 18:07:25 +0530

    new example added in test.feature

--- src/test/java/starter/steps/LoginSteps.java
+++ src/test/java/starter/steps/LoginSteps.java
@@ -1,15 +1,10 @@
 package starter.steps;
 
 import net.serenitybdd.annotations.Step;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import starter.pageObjects.LoginPage;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class LoginSteps {
 
-    private static final Logger log = LoggerFactory.getLogger(LoginSteps.class);
     LoginPage loginPage;
 
     @Step
